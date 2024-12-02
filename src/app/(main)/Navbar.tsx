337d8776c095d5b55ef2656095d450cac233515f
JereUR <jeremias.jdv@gmail.com>
2024-12-01 21:04:14 -0300

    feat: Shadow incremented

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
   const { theme } = useTheme()
 
   return (
-    <header className="shadow-sm ">
+    <header className="shadow-md">
       <div className="max-w-7xl mx-auto p-3 flex items-center justify-between gap-3">
         <Link href='/resumes' className="flex items-center gap-2">
           <Image src={logo} alt="Logo" width={35} height={35} className="rounded-sm" />
