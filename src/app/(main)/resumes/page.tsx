90b56389a2ec881292ecb921513656e02b3855b1
JereUR <jeremias.jdv@gmail.com>
2024-12-01 21:05:20 -0300

    feat: Resumes page link added to editor section

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,7 +1,22 @@
+import { PlusSquare } from "lucide-react"
+import { Metadata } from "next"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+
+export const metadata: Metadata = {
+  title: "Your resumes"
+}
+
 export default function ResumesPage() {
   return (
-    <div>
-      <h1>Your resumes here</h1>
-    </div>
+    <main className="max-w-7xl mx-auto w-full px-3 py-6 space-y-6">
+      <Button asChild className="mx-auto flex w-fit gap-2">
+        <Link href="/editor">
+          <PlusSquare className="size-5" />
+          New resume
+        </Link>
+      </Button>
+    </main>
   )
 }
