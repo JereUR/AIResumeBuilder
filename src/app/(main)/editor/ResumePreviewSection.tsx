a5708812815ae9a6cea3ee966cbca2cd0b3d5d96
JereUR <jeremias.jdv@gmail.com>
2024-12-08 02:39:31 -0300

    feat: Border style button and color picker components added

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -1,5 +1,7 @@
 import ResumePreview from "@/components/ResumePreview"
 import { ResumeValues } from "@/lib/validation"
+import ColorPicker from "./ColorPicker"
+import BorderStyleButton from "./BorderStyleButton"
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues
@@ -7,7 +9,11 @@ interface ResumePreviewSectionProps {
 }
 export default function ResumePreviewSection({ resumeData, setResumeData }: ResumePreviewSectionProps) {
   return (
-    <div className="hidden w-1/2 md:flex">
+    <div className="group relative hidden w-1/2 md:flex">
+      <div className="opacity-30 xl:opacity-100 absolute left-1 top-1 flex flex-col gap-3 flex-none lg:left-3 lg:top-3 transition-opacity group-hover:opacity-100 ">
+        <ColorPicker color={resumeData.colorHex} onChange={(color) => setResumeData({ ...resumeData, colorHex: color.hex })} />
+        <BorderStyleButton borderStyle={resumeData.borderStyle} onChange={(borderStyle) => setResumeData({ ...resumeData, borderStyle })} />
+      </div>
       <div className="flex w-full justify-center overflow-y-auto bg-gray-300 dark:bg-accent/40 p-3">
         <ResumePreview resumeData={resumeData} className="max-w-2xl shadow-md" />
       </div>
