3246d79d90ebef7d801425516ef3c5f56ae0cd92
JereUR <jeremias.jdv@gmail.com>
2025-05-05 22:44:30 -0300

    feat: Saving data state used

--- src/app/(main)/editor/Footer.tsx
+++ src/app/(main)/editor/Footer.tsx
@@ -2,16 +2,18 @@ import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import { steps } from "./steps"
-import { FileUserIcon, PenLineIcon } from "lucide-react"
+import { FileUserIcon, Loader2, PenLineIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 interface FooterProps {
   currentStep: string
   setCurrentStep: (step: string) => void
   showSmResumePreview: boolean
   setShowSmResumePreview: (show: boolean) => void
+  isSaving: boolean
 }
 
-export default function Footer({ currentStep, setCurrentStep, showSmResumePreview, setShowSmResumePreview }: FooterProps) {
+export default function Footer({ currentStep, setCurrentStep, showSmResumePreview, setShowSmResumePreview, isSaving }: FooterProps) {
   const previousStep = steps.find(
     (_, index) => steps[index + 1]?.key === currentStep,
   )?.key
@@ -47,7 +49,9 @@ export default function Footer({ currentStep, setCurrentStep, showSmResumePrevie
           <Button variant="secondary" asChild>
             <Link href="/resumes">Close</Link>
           </Button>
-          <p className="text-muted-foreground opacity-0">Saving...</p>
+          <p className={cn("flex gap-1 items-center text-muted-foreground opacity-0", isSaving && "opacity-100")}>
+            <Loader2 className='animate-spin' /> Saving...
+          </p>
         </div>
       </div>
     </footer>
