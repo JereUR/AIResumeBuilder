5941651fdcd2a738d0876042f90335fa9130b10a
JereUR <jeremias.jdv@gmail.com>
2025-05-06 22:05:56 -0300

    feat: Obtener resume a partir de resumeId pasado por searchParams agregado

--- src/app/(main)/editor/page.tsx
+++ src/app/(main)/editor/page.tsx
@@ -1,10 +1,32 @@
 import { Metadata } from "next"
 import ResumeEditor from "./ResumeEditor"
+import prisma from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+import { resumeDataInclude } from "@/lib/types"
+
+interface PageProps {
+  searchParams: Promise<{ resumeId?: string }>
+}
 
 export const mettadat: Metadata = {
   title: "Design your resume",
 }
 
-export default function EditorPage() {
-  return <ResumeEditor />
+export default async function EditorPage({ searchParams }: PageProps) {
+  const { resumeId } = await searchParams
+
+  const { userId } = await auth()
+
+  if (!userId) {
+    throw new Error("User not authenticated")
+  }
+
+  const resumeToEdit = resumeId ? await prisma.resume.findUnique({
+    where: {
+      id: resumeId, userId
+    },
+    include: resumeDataInclude
+  }) : null
+
+  return <ResumeEditor resumeToEdit={resumeToEdit} />
 }
