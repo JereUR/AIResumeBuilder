ac2798ccfdb0be1e80f001bc9f84df0b7cfcd997
JereUR <jeremias.jdv@gmail.com>
2025-05-06 22:04:58 -0300

    feat: Images desde Vercel storage agregadas a whitelist

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "4mb",
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "7to32hh3fmdbb3jf.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 }
 
 export default nextConfig
