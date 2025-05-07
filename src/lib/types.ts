6ca278b79bf375cfa6203ebb8100a6b89b679b2a
JereUR <jeremias.jdv@gmail.com>
2025-05-06 22:05:37 -0300

    feat: Tipado para data de resume para editar agregado

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,18 @@
+import { Prisma } from "@prisma/client"
 import { ResumeValues } from "./validation"
 
 export interface EditorFormProps {
   resumeData: ResumeValues
   setResumeData: (resumeData: ResumeValues) => void
 }
+
+export const resumeDataInclude = {
+  workExperiences: true,
+  educations: true,
+  languages: true,
+  personalProjects: true,
+} satisfies Prisma.ResumeInclude
+
+export type ResumeServerData = Prisma.ResumeGetPayload<{
+  include: typeof resumeDataInclude
+}>
