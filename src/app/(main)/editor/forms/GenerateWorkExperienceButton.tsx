3f00d37e2bdd7f51a144fbbb67d5eea90010e83f
JereUR <jeremias.jdv@gmail.com>
2025-05-08 22:14:18 -0300

    Added a button to AI-generate a work experience from the user's description.

---
+++ src/app/(main)/editor/forms/GenerateWorkExperienceButton.tsx
@@ -0,0 +1,96 @@
+import { WandSparkles } from "lucide-react"
+import { useState } from "react"
+import { Form, useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import { useToast } from "@/hooks/use-toast"
+import { GenerateWorkExperienceInput, generateWorkExperienceSchema, WorkExperience } from "@/lib/validation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { generateWorkExperience } from "./actions"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Textarea } from "@/components/ui/textarea"
+import LoadingButton from "@/components/LoadingButton"
+
+interface GenerateWorkExperienceButtonProps {
+  onWorkExperienceGenerated: (workExperience: WorkExperience) => void
+}
+
+export default function GenerateWorkExperienceButton({ onWorkExperienceGenerated }: GenerateWorkExperienceButtonProps) {
+  const [showInputDialog, setShowInputDialog] = useState(false)
+
+  return (
+    <>
+      <Button variant='outline' type="button" onClick={() => setShowInputDialog(true)}>
+        <WandSparkles className="size-4" />
+        Smart fill (AI)
+      </Button>
+      <InputDialog open={showInputDialog} onOpenChange={setShowInputDialog} onWorkExperienceGenerated={(workExperience) => {
+        onWorkExperienceGenerated(workExperience)
+        setShowInputDialog(false)
+      }} />
+    </>
+  )
+}
+
+interface InputDialogProps {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  onWorkExperienceGenerated: (workExperience: WorkExperience) => void
+}
+
+function InputDialog({ open, onOpenChange, onWorkExperienceGenerated }: InputDialogProps) {
+  const { toast } = useToast()
+
+  const form = useForm<GenerateWorkExperienceInput>({
+    resolver: zodResolver(generateWorkExperienceSchema),
+    defaultValues: {
+      description: "",
+    }
+  })
+
+  async function onSubmit(input: GenerateWorkExperienceInput) {
+    try {
+      toast({
+        description: "Functionality not implemented due to lack of budget",
+      })
+
+      const response = await generateWorkExperience(input)
+      onWorkExperienceGenerated(response)
+
+    } catch (error) {
+      console.error(error)
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again."
+      })
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate work experience</DialogTitle>
+          <DialogDescription>
+            Describe this work experience and the AI will generate and optimized entry for you
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField control={form.control} name="description" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} placeholder={`E.g. "from nov 2019 to dec 2020 I worked at Google as a software engineer, task were: ..."`} autoFocus />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <LoadingButton type="submit" loading={form.formState.isSubmitting}>Generate</LoadingButton>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
