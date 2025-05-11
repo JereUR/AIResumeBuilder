f0c405051634610c17d075e7ae68738ffbb65f0a
JereUR <jeremias.jdv@gmail.com>
2025-05-11 20:25:13 -0300

    feat: Button color updated

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -42,7 +42,7 @@ export default async function ResumesPage() {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
+      <Button asChild className="mx-auto flex w-fit gap-2 bg-primary/70 dark:bg-primary">
         <Link href="/editor">
           <PlusSquare className="size-5" />
           New resume
